05688d2172cdfd1c82c45040fde5f65a24f1f95c
Will Harris <Harriswill22@yahoo.com>
2018-12-07 13:35:15 -0500

    adds components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="notes-app"></div>
+        <div className="search-bar">
+          <input className="notes-List"></input>
+          <ul>
+            <li><a>note#1</a></li>
+            <li><a>note#2</a></li>
+            <li><a>note#3</a></li>
+            <li><a>note#4</a></li>
+          </ul>
+        </div>
+        <div className="editor-window">
+        it was the worst of times it was the best
+        </div>
       </div>
     );
   }
