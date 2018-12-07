fdcf7dfabd13ee31a8c0e68b87fe79d4888e9a1b
Will Harris <Harriswill22@yahoo.com>
2018-12-07 13:37:31 -0500

    adds components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import EditorWindow from './Editorwindow'
 
 class App extends Component {
   render() {
---
+++ src/notesapp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NotesApp extends Component {
+    constructor(props){
+    super(props)
+    state = {  }
+    }
+
+
+    render() { 
+        return (  );
+    }
+}
+
+export default NotesApp;
